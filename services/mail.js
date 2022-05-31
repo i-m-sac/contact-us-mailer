408000b70ebc7e7952e93ec00e3d0565d239d3a7
Sac <sac@9thimpact.com>
2022-05-31 09:48:15 +0530

    - Added phone as mandatory field

--- services/mail.js
+++ services/mail.js
@@ -13,15 +13,15 @@ module.exports.sendMail = async (req) => {
     console.log('Empty req', req.body)
     throw new Error('Empty request body')
   }
-  const { from, name, subject, message } = req.body
-  if (!(from && name && message)) {
+  const { from, name, subject, message, phone } = req.body
+  if (!(from && name && message && phone)) {
     throw new Error('Missing mandatory params')
   }
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_RECIPIENT,
     subject: template.subject.replace('__name__', name).replace('__subject__', subject),
-    html: template.body.replace('__name__', name).replace('__email__', from).replace('__message__', message),
+    html: template.body.replace('__name__', name).replace('__email__', from).replace('__message__', message).replace('__phone__', phone),
     replyTo: from
   }
   const response = await smtpTransport.sendMail(mailOptions).catch(err => {
